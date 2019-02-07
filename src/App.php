be0d7e8aa811fd531d4ee2d8b942e5812f320841
Matt Light <matt@kelvin.education>
2019-02-06 16:30:18 -0800

    Introduce `AppContext`
    Rather than the Api and Web classes extending App, we are transitioning
    to a 'App has a context' relationship.  We need a `Worker` context and
    some of the `App` logic (HTTP Request/Response handling) is not directly
    relevant in such a context.  By transitioning to a 'has a' relationship,
    the Api and Web contexts can re-use code without forcing the Worker to
    also share the HTTP logic.

--- src/App.php
+++ src/App.php
@@ -19,89 +19,26 @@ class App
      */
     private static $error_handlers = [];
 
-    public static function run()
+    public static function run(AppContext $app_context)
     {
         option('env', ENV_PRODUCTION);
         option('root_dir', dirname(__DIR__));
-        option('views_dir', option('root_dir') . '/views');
+
+        $app = new self();
 
         set_error_handler([__CLASS__, 'errorException'], E_ALL);
         set_exception_handler([__CLASS__, 'errorDispatcher']);
         register_shutdown_function([__CLASS__, 'handleFatal']);
 
-        $app = new static();
-        $app->registerErrorHandler(SERVER_ERROR, [$app, 'defaultErrorHandler']);
-        $app->configure();
+        $app->registerErrorHandler(SERVER_ERROR, [$app_context, 'defaultErrorHandler']);
 
-        if ($session = option('session')) {
-            $app->startSession($session);
-        }
-
-        $app->initialize();
-
-        try {
-            $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-            [$page_class, $vars] = page($path, 'Home', '\K\Pages\\');
-
-            $response = new \K\Response();
-            $request = new \K\Request();
-            if (!class_exists($page_class)) {
-                $app->handleNotFound($response);
-                $response->send();
-                return;
-            }
-
-            $page = call_user_func(["$page_class", 'factory']);
-            $page->__invoke($response, $request, $vars);
-            $response->send();
-        } catch (\Throwable $e) {
-            $app->defaultErrorHandler($e);
-        }
+        $app_context->configure($app);
+        $app_context->run();
     }
 
-    protected function configure() { }
-    protected function initialize() {  }
-    protected function handleServerError(\K\ResponseWriterInterface $w, \Throwable $e) { }
-    protected function handleNotFound(\K\ResponseWriterInterface $w) { }
-    protected function handleNotAllowed(\K\ResponseWriterInterface $w) { }
-
-    protected function registerErrorHandler(int $err_code, callable $handler)
+    public function registerErrorHandler(int $err_code, callable $handler)
     {
         self::$error_handlers[$err_code] = $handler;
-    }
-
-    private function defaultErrorHandler(\Throwable $e)
-    {
-        $w = new \K\Response();
-        $w->withStatus(500);
-
-        $this->handleServerError($w, $e);
-        $w->send();
-    }
-
-    private function startSession(array $session)
-    {
-        if (isset($_COOKIE[$session['name']])) {
-            setcookie(
-                $session['name'],
-                $_COOKIE[$session['name']],
-                time() + $session['lifetime'],
-                $session['path'],
-                $session['domain'],
-                $session['secure'],
-                $session['httponly']
-            );
-        }
-        session_start([
-            'name'            => $session['name'],
-            'cookie_lifetime' => $session['lifetime'],
-            'cookie_path'     => $session['path'],
-            'cookie_domain'   => $session['domain'],
-            'cookie_secure'   => $session['secure'],
-            'cookie_httponly' => $session['httponly'],
-            'cache_limiter'   => '',
-            'gc_maxlifetime'  => $session['lifetime'] * 3,
-        ]);
     }
 
     /**
---
+++ src/HttpContext.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace K;
+
+use function K\{option};
+
+class HttpContext extends AppContext
+{
+    /**
+     * @var AppContext $child_context
+     */
+    private $child_context;
+
+    public function __construct(AppContext $child_context = null)
+    {
+        parent::__construct();
+
+        $this->child_context = $child_context ?? new AppContext();
+    }
+
+    public function configure(App $app)
+    {
+        option('views_dir', option('root_dir') . '/views');
+
+        $this->child_context->configure($app);
+
+        if ($session = option('session')) {
+            $this->startSession($session);
+        }
+    }
+
+    public function run()
+    {
+        try {
+            $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+            [$page_class, $vars] = page($path, 'Home', '\K\Pages\\');
+
+            $response = new \K\Response();
+            $request = new \K\Request();
+            if (!class_exists($page_class)) {
+                $this->handleNotFound($response);
+                $response->send();
+                return;
+            }
+
+            $page = call_user_func(["$page_class", 'factory']);
+            $page->__invoke($response, $request, $vars);
+            $response->send();
+        } catch (\Throwable $e) {
+            $this->defaultErrorHandler($e);
+        }
+    }
+
+    public function defaultErrorHandler(\Throwable $e)
+    {
+        $w = new \K\Response();
+        $w->withStatus(500);
+
+        $this->handleServerError($w, $e);
+        $w->send();
+    }
+
+    private function handleServerError(\K\ResponseWriterInterface $w, \Throwable $e)
+    {
+        if (method_exists($this->child_context, 'handleServerError')) {
+            $this->child_context->handleServerError($w, $e);
+        }
+    }
+
+    private function handleNotFound(\K\ResponseWriterInterface $w)
+    {
+        if (method_exists($this->child_context, 'handleNotFound')) {
+            $this->child_context->handleNotFound($w);
+        }
+    }
+
+    private function startSession(array $session)
+    {
+        if (isset($_COOKIE[$session['name']])) {
+            setcookie(
+                $session['name'],
+                $_COOKIE[$session['name']],
+                time() + $session['lifetime'],
+                $session['path'],
+                $session['domain'],
+                $session['secure'],
+                $session['httponly']
+            );
+        }
+        session_start([
+            'name'            => $session['name'],
+            'cookie_lifetime' => $session['lifetime'],
+            'cookie_path'     => $session['path'],
+            'cookie_domain'   => $session['domain'],
+            'cookie_secure'   => $session['secure'],
+            'cookie_httponly' => $session['httponly'],
+            'cache_limiter'   => '',
+            'gc_maxlifetime'  => $session['lifetime'] * 3,
+        ]);
+    }
+}
