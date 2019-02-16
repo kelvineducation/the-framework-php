77d0abf1fdf69d32563eab2741aa488eed82029c
Matt Light <matt@kelvin.education>
2019-02-15 21:25:34 -0800

    Remove JSON params from $_POST
    The JSON parameters are stored in the class' internals and processed
    that way, so they do not need to be stored in the $_POST superglobal.

--- src/Request.php
+++ src/Request.php
@@ -7,13 +7,6 @@ class Request implements RequestInterface, Form\RequestInterface
     private $headers;
     private $params;
 
-    public function __construct()
-    {
-        if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_SERVER['CONTENT_TYPE'] === 'application/json') {
-            $_POST = json_decode(file_get_contents('php://input'), true);
-        }
-    }
-
     /**
      * @param string $key
      * @param mixed $default
